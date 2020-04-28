9df98770be535f35924bde030a3d786f5b89c42e
Adrian Etenyi <etadriano2@gmail.com>
2020-04-28 09:32:15 +0300

    test class to test news behaviour

--- tests/test_news.py
+++ tests/test_news.py
@@ -1,7 +1,7 @@
 import unittest
-from app.models import Articles
+from app.models import News
 
-class TestArticles(unittest.TestCase):
+class TestNews(unittest.TestCase):
     '''
     Test class to test the behavior of the articles class
     '''
@@ -9,15 +9,26 @@ class TestArticles(unittest.TestCase):
         '''
         Test class to run before other tests
         '''
-        self.new_article = Articles('Title goes here','Title is great ...','https://google.com/images','2018-05-12T13:31:03Z', 'liz', 'buzzfeed.com')
+        self.new_news_source = News('abc','liz','https://abc.com/','abc news is the best source', 'usa', 'general', 'abc-news')
     
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_article,Articles))
+        '''
+        '''
+        self.assertTrue(isinstance(self.new_news_source,News))
     
     def test_to_check_instance_variables(self):
-        self.assertEquals(self.new_article.title,'Title goes here')
-        self.assertEquals(self.new_article.description,'Title is great...')
-        self.assertEquals(self.new_article.image,'https://google.com/images')
-        self.assertEquals(self.new_article.publishedAt,'h2018-05-12T13:31:03Z')
-        self.assertEquals(self.new_article.author,'liz')
-        self.assertEquals(self.new_article.url,'buzzfeed.com')
+        '''
+        '''
+        self.assertEquals(self.new_news_source.name,'abc')
+        self.assertEquals(self.new_news_source.author,'liz')
+        self.assertEquals(self.new_news_source.url,'https://abc.com/')
+        self.assertEquals(self.new_news_source.description,'abc news is the best source')
+        self.assertEquals(self.new_news_source.country,'usa')
+        self.assertEquals(self.new_news_source.category,'general')
+        self.assertEquals(self.new_news_source.id,'abc-news')
+        
+        
+        
+
+
+        
